a7af0850238350e2a116020c716a777e2d0bf47b
furti <daniel.furtlehner@gmx.net>
2018-07-29 17:52:40 +0200

    Fixes duplicate vertices and self intersections

--- create_box.py
+++ create_box.py
@@ -7,17 +7,42 @@ from PySide import QtGui
 import lithophane_utils, toolbars
 from utils.timer import Timer, computeOverallTime
 
+def createBottomRectangle(lines):
+    facets = []
+
+    for lineNumber, actualLine in enumerate(lines):
+        # No need to perform operations on the last line at all
+        if lineNumber == len(lines) - 1:
+            break
+        
+        nextLine = lines[lineNumber + 1]
+
+        for rowNumber, actualPoint in enumerate(actualLine):
+            # No need to perform operations on the last row
+            if rowNumber == len(actualLine) - 1:
+                break
+            
+            bottomLeft = lithophane_utils.vectorAtGround(actualPoint)
+            bottomRight = lithophane_utils.vectorAtGround(actualLine[rowNumber + 1]) # next row same line
+            topRight = lithophane_utils.vectorAtGround(nextLine[rowNumber + 1]) # next row next line
+            topLeft = lithophane_utils.vectorAtGround(nextLine[rowNumber]) # same row next line
+
+            facets.extend([bottomLeft, bottomRight, topLeft])
+            facets.extend([bottomRight, topRight, topLeft])
+
+    return Mesh.Mesh(facets)
+
 def makeBlockBase(lines):
     facets = []
 
     # add the bottom rectangle
-    bottomLeftCorner = lithophane_utils.vectorAtGround(lines[0][0])
-    bottomRightCorner = lithophane_utils.vectorAtGround(lines[0][-1])
-    topRightCorner = lithophane_utils.vectorAtGround(lines[-1][-1])
-    topLeftCorner = lithophane_utils.vectorAtGround(lines[-1][0])
+    # bottomLeftCorner = lithophane_utils.vectorAtGround(lines[0][0])
+    # bottomRightCorner = lithophane_utils.vectorAtGround(lines[0][-1])
+    # topRightCorner = lithophane_utils.vectorAtGround(lines[-1][-1])
+    # topLeftCorner = lithophane_utils.vectorAtGround(lines[-1][0])
 
-    facets.extend([bottomLeftCorner, topLeftCorner, bottomRightCorner])
-    facets.extend([bottomRightCorner, topLeftCorner, topRightCorner])
+    # facets.extend([bottomLeftCorner, topLeftCorner, bottomRightCorner])
+    # facets.extend([bottomRightCorner, topLeftCorner, topRightCorner])
 
     for lineNumber, actualLine in enumerate(lines):
         # Need to process all the points on the first and last line
@@ -118,11 +143,23 @@ class CreateGeometryCommand:
         block = makeBlockBase(lithophaneImage.lines)
         timers[-1].stop()
         lithophane_utils.processEvents()
+
+        timers.append(Timer('Create Bottom Plane'))
+        bottomPlane = createBottomRectangle(lithophaneImage.lines)
+        timers[-1].stop()
+        lithophane_utils.processEvents()
         
         timers.append(Timer('Merge Meshes'))
         imageMesh = Mesh.Mesh()
         imageMesh.addMesh(imagePlane)
         imageMesh.addMesh(block)
+        imageMesh.addMesh(bottomPlane)
+        timers[-1].stop()
+        lithophane_utils.processEvents()
+
+        timers.append(Timer('Removing Duplicate Points'))
+        imageMesh.removeDuplicatedPoints()
+        lithophane_utils.recomputeView()
         timers[-1].stop()
         lithophane_utils.processEvents()
 
@@ -136,6 +173,8 @@ class CreateGeometryCommand:
         Mesh.show(imageMesh, 'Image')
         lithophane_utils.recomputeView()
         timers[-1].stop()
+        lithophane_utils.processEvents()
+
 
         FreeCAD.Console.PrintMessage('Creating Boxy image took %.3f s' % (computeOverallTime(timers)))
 
